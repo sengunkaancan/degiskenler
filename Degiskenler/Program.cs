ad443d553513522507fbd884cbabeef4846733b7
sengunkaancan <kaancan_90@hotmail.com>
2019-10-26 14:14:29 +0300

    ders sonu notları

--- Degiskenler/Program.cs
+++ Degiskenler/Program.cs
@@ -10,6 +10,15 @@ namespace Degiskenler
     {
         static void Main(string[] args)
         {
-        }
+            byte s1 = 255;
+            sbyte s2 = 123;
+            int s3 = 10; // MinValue = -2147483648 & MaxValue = 2147483647;
+            uint s4 = 15; // MaxValue = 4294967295 & MinValue = 0;;
+            long s5 = 200; // MaxValue = 9223372036854775807 & MinValue = -9223372036854775808;
+            ulong s6 = 250; // MaxValue = 18446744073709551615 & MinValue = 0;
+
+
+
+    }
     }
 }
